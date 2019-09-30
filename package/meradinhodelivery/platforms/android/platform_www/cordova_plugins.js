d76b2954070d8d153ce75dd4d9974685a9bd38fd
xrud <paulosergioduffjob@gmail.com>
2019-09-30 13:54:29 +0000

    GitFlash

--- package/meradinhodelivery/platforms/android/platform_www/cordova_plugins.js
+++ package/meradinhodelivery/platforms/android/platform_www/cordova_plugins.js
@@ -8,13 +8,46 @@ module.exports = [
       "cordova.InAppBrowser.open",
       "window.open"
     ]
+  },
+  {
+    "id": "org.apache.cordova.geolocation.Coordinates",
+    "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+    "pluginId": "org.apache.cordova.geolocation",
+    "clobbers": [
+      "Coordinates"
+    ]
+  },
+  {
+    "id": "org.apache.cordova.geolocation.PositionError",
+    "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+    "pluginId": "org.apache.cordova.geolocation",
+    "clobbers": [
+      "PositionError"
+    ]
+  },
+  {
+    "id": "org.apache.cordova.geolocation.Position",
+    "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+    "pluginId": "org.apache.cordova.geolocation",
+    "clobbers": [
+      "Position"
+    ]
+  },
+  {
+    "id": "org.apache.cordova.geolocation.geolocation",
+    "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+    "pluginId": "org.apache.cordova.geolocation",
+    "clobbers": [
+      "navigator.geolocation"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-inappbrowser": "3.1.0",
-  "cordova-plugin-whitelist": "1.3.4"
+  "cordova-plugin-whitelist": "1.3.4",
+  "org.apache.cordova.geolocation": "0.3.6"
 };
 // BOTTOM OF METADATA
 });
--- package/meradinhodelivery/platforms/ios/platform_www/cordova_plugins.js
+++ package/meradinhodelivery/platforms/ios/platform_www/cordova_plugins.js
@@ -14,7 +14,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-inappbrowser": "3.1.0",
-  "cordova-plugin-whitelist": "1.3.4"
+  "cordova-plugin-whitelist": "1.3.4",
+  "org.apache.cordova.geolocation": "0.3.6"
 };
 // BOTTOM OF METADATA
 });
