2ada910894deade9b2bfc18d2cc2e02cf713f6c1
xrud <paulosergioduffjob@gmail.com>
2019-09-30 14:32:24 +0000

    GitFlash

--- package/meradinhodelivery/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ package/meradinhodelivery/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -10,36 +10,18 @@ module.exports = [
     ]
   },
   {
-    "id": "org.apache.cordova.geolocation.Coordinates",
-    "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
-    "pluginId": "org.apache.cordova.geolocation",
-    "clobbers": [
-      "Coordinates"
-    ]
-  },
-  {
-    "id": "org.apache.cordova.geolocation.PositionError",
-    "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
-    "pluginId": "org.apache.cordova.geolocation",
-    "clobbers": [
-      "PositionError"
-    ]
-  },
-  {
-    "id": "org.apache.cordova.geolocation.Position",
-    "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
-    "pluginId": "org.apache.cordova.geolocation",
-    "clobbers": [
-      "Position"
-    ]
-  },
-  {
-    "id": "org.apache.cordova.geolocation.geolocation",
-    "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
-    "pluginId": "org.apache.cordova.geolocation",
+    "id": "cordova-plugin-geolocation.geolocation",
+    "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+    "pluginId": "cordova-plugin-geolocation",
     "clobbers": [
       "navigator.geolocation"
     ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.PositionError",
+    "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "runs": true
   }
 ];
 module.exports.metadata = 
@@ -47,7 +29,7 @@ module.exports.metadata =
 {
   "cordova-plugin-inappbrowser": "3.1.0",
   "cordova-plugin-whitelist": "1.3.4",
-  "org.apache.cordova.geolocation": "0.3.6"
+  "cordova-plugin-geolocation": "4.0.3-dev"
 };
 // BOTTOM OF METADATA
 });
--- package/meradinhodelivery/platforms/ios/platform_www/cordova_plugins.js
+++ package/meradinhodelivery/platforms/ios/platform_www/cordova_plugins.js
@@ -8,6 +8,38 @@ module.exports = [
       "cordova.InAppBrowser.open",
       "window.open"
     ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.Coordinates",
+    "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "clobbers": [
+      "Coordinates"
+    ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.PositionError",
+    "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "clobbers": [
+      "PositionError"
+    ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.Position",
+    "file": "plugins/cordova-plugin-geolocation/www/Position.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "clobbers": [
+      "Position"
+    ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.geolocation",
+    "file": "plugins/cordova-plugin-geolocation/www/geolocation.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "clobbers": [
+      "navigator.geolocation"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -15,7 +47,7 @@ module.exports.metadata =
 {
   "cordova-plugin-inappbrowser": "3.1.0",
   "cordova-plugin-whitelist": "1.3.4",
-  "org.apache.cordova.geolocation": "0.3.6"
+  "cordova-plugin-geolocation": "4.0.3-dev"
 };
 // BOTTOM OF METADATA
 });
